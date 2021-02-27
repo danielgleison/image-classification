bfc5fc86d39c14d2f9c16b88d03cd94175e3bd47
Daniel Gleison <dgmlira@gmail.com>
2021-02-26 21:32:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ Aplicação acadêmica de detecção de tumor cerebral utilizado Tensor Flow e K
 O treinamento do modelo de classificação foi baseado no dataset [Brain MRI Images for Brain Tumor Detection]
 (https://www.kaggle.com/navoneel/brain-mri-images-for-brain-tumor-detection), disponível no Kaggle,
 que possui 253 imagens de Ressonância Magnética (RM), sendo 155 com tumor e 98 sem tumor.\n
-Código adaptado de Kimaru Thagana, disponível no [GitHub](https://github.com/KimaruThagna/Picture-lytics).\n
+
 Carregue a imagem de RM para predição do diagnóstico: com tumor ou sem tumor.
 
 
