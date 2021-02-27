3d564ab11c06df8e10b334758b9d7562388cc189
Daniel Gleison <dgmlira@gmail.com>
2021-02-26 21:33:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,6 @@ Carregue a imagem de RM para predição do diagnóstico: com tumor ou sem tumor.
 
 
 
-
-
-
 from img_classification import teachable_machine_classification
 
 uploaded_file = st.file_uploader("Selecione a imagem de RM ...", type="jpg")
@@ -34,6 +31,37 @@ if uploaded_file is not None:
         st.write("A RM cerebral analisada tem tumor.")
     else:
         st.write("A RM cerebral analisada não tem tumor.")
+
+
+st.sidebar.write("""
+
+Daniel Gleison M. Lira
+--
+Mestrando em Ciências da Computação
+Universidade Estadual do Ceará
+[daniel.gleison@aluno.uece.br](mailto:daniel.gleison@aluno.uece.br)
+[github.com/danielgleison](https://github.com/danielgleison)
+[linkedin.com/in/danielgleison] (https://www.linkedin.com/in/danielgleison/)
+""")
+
+
+# Portfolio
+st.sidebar.write("""
+
+Portfólio de Apps Web
+--
+* [Predição de Diabetes](http://dg-diabetes-prediction.herokuapp.com/)
+* [Plaforma IoT](http://dg-iot.herokuapp.com)
+* [Censo Escolar](http://censo-escolar.herokuapp.com/)
+""")
+
+
+# QR-Code
+image = Image.open('qrcode.jpg')
+st.sidebar.image(image, width=100, use_column_width=False, caption='v0.19.200221')
+
+
+from img_classification import teachable_machine_classification
 
 
 
